9c555bfa3879bc04d0fe395a94ca10f7515ece07
Toby E Chin <tchin@brotherhoodmutual.com>
2017-07-10 09:23:20 -0400

    Convert some of the JS to ES6 and update the mac app, include a Linux app (untested), and update UI to allow for larger viewing

--- Binary Clock-darwin-x64/Binary Clock.app/Contents/Resources/app/js/clock.js
+++ Binary Clock-darwin-x64/Binary Clock.app/Contents/Resources/app/js/clock.js
@@ -1,50 +1,51 @@
-function binaryClock() {
-  var timeArr;
-  setInterval(
-    function() {
-      timeArr = buildTimeArray();
-      buildClock(timeArr);
-    }, 1000);
-}
+const BinaryClock = {
+  initialize: function() {
+    let timeArr;
+    setInterval(() => {
+        timeArr = this.buildTimeArray();
+        this.buildClock(timeArr);
+      }, 5);
+  },
 
-function buildClock(timeArray) {
-  var intArray, value;
-  // Loop over the columns, (hours, minutes, seconds)
-  for (var i = 0; i < timeArray.length; i++) {
-    value = converToBinary(timeArray[i]);
-    intArray = value.split('').reverse();
-    // Loop over the values of hours and minutes, and seconds
-    for(var si = 0; si < 4; si++) {
-      renderDots(i, si, intArray[si]);
+  buildClock: function(timeArray) {
+    let intArray, value;
+    // Loop over the columns, (hours, minutes, seconds)
+    for (let i = 0; i < timeArray.length; i++) {
+      value = this.convertToBinary(timeArray[i]);
+      intArray = value.split('').reverse();
+      // Loop over the values of hours and minutes, and seconds
+      for(let si = 0; si < 4; si++) {
+        this.renderDots(i, si, intArray[si]);
+      }
+    }
+  },
+
+  buildTimeArray: function() {
+    const now = new Date();
+    let h = this.pad(now.getHours().toString()).split(''),
+        m = this.pad(now.getMinutes().toString()).split(''),
+        s = this.pad(now.getSeconds().toString()).split(''),
+        timeArr = [];
+    console.log('now: ' + now);
+    return timeArr.concat(h).concat(m).concat(s);
+  },
+
+  convertToBinary: (value) => parseInt(value).toString(2),
+
+  pad: (number) => (`0000${number}`).slice(-2),
+
+  renderDots: function (index, subindex, value) {
+    let el = document.querySelector(`[data-index="${index}"] [data-subindex="${subindex}"`);
+    if (value === '1') {
+      el.classList.remove('off');
+    } else if (!!el) {
+      el.classList.add('off');
     }
   }
-}
+};
 
-function buildTimeArray() {
-  var now, h, m, s, timeArr;
-  now = new Date();
-  h = pad(now.getHours().toString()).split('');
-  m = pad(now.getMinutes().toString()).split('');
-  s = pad(now.getSeconds().toString()).split('');
-  timeArr = [];
-  console.log('now: ' + now);
-  return timeArr.concat(h).concat(m).concat(s);
-}
+console.log(BinaryClock.convertToBinary('56'));
+console.log(BinaryClock.pad('34'));
+console.log(BinaryClock.buildTimeArray());
 
-function converToBinary(value) {
-  return parseInt(value).toString(2);
-}
-
-function pad(num){
-  return ('0000' + num).slice(-2);
-}
-
-function renderDots (index, subindex, value) {
-  var el;
-  el = document.querySelector('[data-index="' + index + '"] [data-subindex="' + subindex + '"');
-  if (value === '1') {
-    el.classList.remove('off');
-  } else if (!!el) {
-    el.classList.add('off');
-  }
-}
+if (document != undefined) { BinaryClock.initialize(); }
