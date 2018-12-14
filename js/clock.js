7fe1a57253988a9d0dabbd2c8e7cc57c2db76a95
Toby E Chin <tchin@brotherhoodmutual.com>
2018-12-14 16:28:09 -0500

    Remove Ramda dependency for vanilla implementation

--- js/clock.js
+++ js/clock.js
@@ -2,25 +2,26 @@ const BinaryClock = {
   _utilities: {
     toArray: (str) => str.split(''),
     padWithZeros: function (y = 2) { return function (x) { return (`0000${x}`).slice(- y); }; },
-    convertToBinary: (x) => parseInt(x).toString(2)
+    convertToBinary: (x) => parseInt(x).toString(2),
+    pipe: (first, ...more) => more.reduce((acc, curr) => (...agrs) => curr(acc(...agrs)), first)
   },
 
   buildTime: function() {
     f = BinaryClock._utilities;
     const now = new Date();
     return {
-      hours: R.pipe(f.padWithZeros(), f.toArray)(now.getHours()),
-      minutes: R.pipe(f.padWithZeros(), f.toArray)(now.getMinutes()),
-      seconds: R.pipe(f.padWithZeros(), f.toArray)(now.getSeconds())
+      hours: f.pipe(f.padWithZeros(), f.toArray)(now.getHours()),
+      minutes: f.pipe(f.padWithZeros(), f.toArray)(now.getMinutes()),
+      seconds: f.pipe(f.padWithZeros(), f.toArray)(now.getSeconds())
     };
   },
 
   clockTime: function () {
     var f = BinaryClock._utilities;
     var timeObj = BinaryClock.buildTime();
-    var convert = R.pipe(f.convertToBinary, f.padWithZeros(4), f.toArray);
-    var mediumConvert = R.pipe(f.convertToBinary, f.padWithZeros(3), f.toArray);
-    var shortConvert = R.pipe(f.convertToBinary, f.padWithZeros(2), f.toArray);
+    var convert = f.pipe(f.convertToBinary, f.padWithZeros(4), f.toArray);
+    var mediumConvert = f.pipe(f.convertToBinary, f.padWithZeros(3), f.toArray);
+    var shortConvert = f.pipe(f.convertToBinary, f.padWithZeros(2), f.toArray);
     return {
       hours: {
         first: shortConvert(timeObj.hours[0]),
